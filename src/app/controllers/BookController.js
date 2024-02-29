8ce0627ddfd9800a9c6d65a63a18b5a9e4abcac9
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2024-02-29 19:46:42 +0700

    book-manager

--- src/app/controllers/BookController.js
+++ src/app/controllers/BookController.js
@@ -4,7 +4,6 @@ const { mongooseToObject } = require('../../util/mongoose');
 class BookController {
     // [GET] /books/:slug
     show(req, res, next) {
-        console.log(req.params.slug);
         Book.findOne({ slug: req.params.slug })
             .then((book) => {
                 res.render('books/show', {
